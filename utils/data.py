f1482fc7017ab5786307b6a908e705fc9079437f
TerryXhx <TerryXhx@outlook.com>
2022-05-17 21:37:59 +0800

    Add author-author mapping, paper-paper mapping and random walk generation function.

--- utils/data.py
+++ utils/data.py
@@ -4,6 +4,7 @@ import random
 import time
 import torch
 from torch_sparse import SparseTensor
+from torch import Tensor 
 from typing import Dict, List, Set, Tuple
 
 # Better to be included in a global config file
@@ -16,7 +17,9 @@ AUTHOR_CNT = 6611
 PAPER_CNT = 79937
 
 AUTHOR_ADJ_PATH = 'data/author_adj.pkl'
+AUTHOR_AUTHOR_MAP_PATH = 'data/author_author_map.pkl'
 PAPER_ADJ_PATH = 'data/paper_adj.pkl'
+PAPER_PAPER_MAP_PATH = 'data/paper_paper_map.pkl'
 BIPARTITE_ADJ_PATH = 'data/bipartite_adj.pkl'
 BIPARTITE_LAP_PATH = 'data/bipartite_lap.pkl'
 AUTHOR_PAPER_MAP_PATH = 'data/author_paper_map.pkl'
@@ -30,11 +33,41 @@ class Data(object):
         self.batch_size = batch_size
         self.n_authors, self.n_papers = AUTHOR_CNT, PAPER_CNT
         self.author_adj_matrix = self.get_author_adj_matrix()
+        self.author_author_map = self.get_author_author_map()
         self.paper_adj_matrix = self.get_paper_adj_matrix()
+        self.paper_paper_map = self.get_paper_paper_map()
         self.paper_embeddings = self.get_paper_embeddings()
         self.author_paper_map = self.get_author_paper_map()
         self.bipartite_adj_matrix, self.bipartite_lap_matrix = self.get_bipartite_matrix()
-    
+
+    def get_author_author_map(self) -> Dict[int, Set[int]]:
+        """Returns the mapping from authors to authors in coauthor network.
+        Returns:
+            The mapping from authors to authors.
+        Example:
+            author 1 has coauthored with author 10 and author 11
+            return {1: {10, 11}, 10: {1}, 11: {1}}
+        """
+        try:
+            t1 = time.time()
+            with open(AUTHOR_AUTHOR_MAP_PATH, 'rb') as f:
+                author_author_map = pickle.load(f)
+            print(f'Load author-author map from {AUTHOR_AUTHOR_MAP_PATH}, time cost: {time.time() - t1: .3f}s')
+        except:
+            t1 = time.time()
+            with open(AUTHOR_GRAPH_PATH, 'r') as f:
+                lines = f.readlines()
+                author_author_map = {author: set() for author in range(AUTHOR_CNT)}
+                for line in lines:
+                    for author, coauthor in [line.strip().split(' ')]:
+                        author_author_map[int(author)].add(int(coauthor))
+                        author_author_map[int(coauthor)].add(int(author))
+            print(f'Build author-author map, time cost: {time.time() - t1: .3f}s')
+            with open(AUTHOR_AUTHOR_MAP_PATH, 'wb') as f:
+                pickle.dump(author_author_map, f)
+
+        return author_author_map
+
     def get_author_adj_matrix(self) -> SparseTensor:
         """Returns the adjacency matrix of the coauthor graph.
         Returns:
@@ -65,6 +98,35 @@ class Data(object):
                 pickle.dump(author_adj_matrix, f)
 
         return author_adj_matrix
+
+    def get_paper_paper_map(self) -> Dict[int, Set[int]]:
+        """Returns the mapping from papers to papers in citation network.
+        Returns:
+            The mapping from papers to papers.
+        Example:
+            paper 1 has cited paper 10, 11
+            return {1: {10, 11}, 10: {1}, 11: {1}}
+        Note: all paper indexes are in range [0, PAPER_CNT)
+        """
+        try:
+            t1 = time.time()
+            with open(PAPER_PAPER_MAP_PATH, 'rb') as f:
+                paper_paper_map = pickle.load(f)
+            print(f'Load paper-paper map from {PAPER_PAPER_MAP_PATH}, time cost: {time.time() - t1: .3f}s')
+        except:
+            t1 = time.time()
+            with open(PAPER_GRAPH_PATH, 'r') as f:
+                lines = f.readlines()
+                paper_paper_map = {paper: set() for paper in range(PAPER_CNT)}
+                for line in lines:
+                    for paper, cited_paper in [line.strip().split(' ')]:
+                        paper_paper_map[int(paper)].add(int(cited_paper))
+                        paper_paper_map[int(cited_paper)].add(int(paper))
+            print(f'Build paper-paper map, time cost: {time.time() - t1: .3f}s')
+            with open(PAPER_PAPER_MAP_PATH, 'wb') as f:
+                pickle.dump(paper_paper_map, f)
+
+        return paper_paper_map
 
     def get_paper_adj_matrix(self) -> SparseTensor:
         """Returns the adjacency matrix of the citation network among papers.
@@ -220,9 +282,55 @@ class Data(object):
             neg_papers.extend(sample_neg_papers_for_author(author, 1))
         
         return authors, pos_papers, neg_papers
+    
+    def generate_random_walk_author(self, t: int) -> Tensor:
+        """Generate the random walk for all authors in coauthor network.
+
+        Args:
+            t (int): the length of random walk (t << N).
+        Returns:
+            random_walk_matrix (Tensor): (AUTHOR_CNT, t)
+        """
+        random_walk_matrix = torch.zeros(size=(AUTHOR_CNT, t), dtype=torch.int32)
+        for author in range(AUTHOR_CNT):
+            random_walk_matrix[author, 0] = author
+            pre = author
+            for i in range(1, t):
+                cur = random.choice(list(self.author_author_map[pre]))
+                random_walk_matrix[author, i] = cur
+                pre = cur
+        return random_walk_matrix
+
+    def generate_random_walk_paper(self, t: int) -> Tensor:
+        """Generate the random walk for all papers in citation network.
+
+        Args:
+            t (int): the length of random walk (t << M).
+        Returns:
+            random_walk_matrix (Tensor): (PAPER_CNT, t)
+        """
+        random_walk_matrix = torch.zeros(size=(PAPER_CNT, t), dtype=torch.int32)
+        for paper in range(PAPER_CNT):
+            random_walk_matrix[paper, 0] = paper
+            pre = paper
+            for i in range(1, t):
+                cur = random.choice(list(self.paper_paper_map[pre]))
+                random_walk_matrix[paper, i] = cur
+                pre = cur
+        return random_walk_matrix
 
 if __name__ == '__main__':
     data_generator = Data(batch_size=10)
-    print(data_generator.author_adj_matrix[0])
-    print(data_generator.paper_adj_matrix[0])
-    print(data_generator.bipartite_adj_matrix[0])
+    # print(data_generator.author_author_map[0])
+    # print(data_generator.paper_paper_map[0])
+    print('--author--')
+    path = data_generator.generate_random_walk_author(t=5)[0]
+    for i in path:
+        print(i, data_generator.author_author_map[int(i)])
+    print('--paper--')
+    path = data_generator.generate_random_walk_paper(t=5)[0]
+    for i in path:
+        print(i, data_generator.paper_paper_map[int(i)])
+    # print(data_generator.author_adj_matrix[0])
+    # print(data_generator.paper_adj_matrix[0])
+    # print(data_generator.bipartite_adj_matrix[0])
