fc684951a71c5958cf044c582798607cfe8b6fa9
pixas <jiangshuyang@sjtu.edu.cn>
2022-05-19 14:18:47 +0800

    fix bugs

--- test.py
+++ test.py
@@ -2,26 +2,30 @@ from models.NGCF import NGCF
 from utils.data import AUTHOR_CNT, PAPER_CNT, Data
 import torch
 import torch.nn as nn
+import pickle
 
 if __name__ == '__main__':
-    a = torch.Tensor([[1, 2], [2, 3], [1, 3]])
-    print(a >= 2)
-    print(torch.sum(a >= 2))
-    b = torch.Tensor([[1, 2], [3, 4]])
-    # print(a * b)
-    d = torch.matmul(a, b.transpose(0, 1))
-    print(d)
+    # a = torch.Tensor([[1, 2], [2, 3], [1, 3]])
+    # print(a >= 2)
+    # print(torch.sum(a >= 2))
+    # b = torch.Tensor([[1, 2], [3, 4]])
+    # # print(a * b)
+    # d = torch.matmul(a, b.transpose(0, 1))
     # print(d)
-    # # print(torch.mul(a, b))
-    # # print(torch.sum(torch.mul(a, b), axis=1))
-    # print(a)
-    c = [[0, 0], [1, 0], [2, 1]]
-    # # print(a[c])
-    e = d[list(zip(*c))]
-    print(e)
-    f = [1, 2]
-    print(a[f])
-    print(torch.norm(a[f]) ** 2)
-    print()
-    print(nn.LogSigmoid()(e))
+    # # print(d)
+    # # # print(torch.mul(a, b))
+    # # # print(torch.sum(torch.mul(a, b), axis=1))
+    # # print(a)
+    # c = [[0, 0], [1, 0], [2, 1]]
+    # # # print(a[c])
+    # e = d[list(zip(*c))]
+    # print(e)
+    # f = [1, 2]
+    # print(a[f])
+    # print(torch.norm(a[f]) ** 2)
+    # print()
+    # print(nn.LogSigmoid()(e))
+    with open("./data/author_vec.pkl", 'rb') as f:
+        x = pickle.load(f)
+    print(x.shape)
     # print(a.shape)
