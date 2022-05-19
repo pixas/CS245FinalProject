069ff392e395a60d954cabcdf5e3d3c094396b4b
pixas <jiangshuyang@sjtu.edu.cn>
2022-05-19 23:30:41 +0800

    modify args

--- train.py
+++ train.py
@@ -10,11 +10,27 @@ from utils.data import Data
 
 
 TRAIN_FILE_TXT = 'data/bipartite_train.txt'
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run General")
+    parser.add_argument('save_dir', type=str, default='checkpoints', help='Directory to save checkpoints')
+    parser.add_argument('--module_type', nargs='?', default='LSTM', help='Module in coauthor and citation network')
+    parser.add_argument('--lr', type=float, default=0.0001, help='Learning rate.')
+    parser.add_argument('--epoch', type=int, default=100, help='Number of epochs')
+    parser.add_argument('--keep_last_epochs', type=int, default=5, help='only keep last epochs')
+    parser.add_argument('--batch_size', type=int, default=8192, help='batch samples for positive and negative samples')
+    parser.add_argument('--embed_dim', type=int, default=512, help='embedding dimension')
+    parser.add_argument('--rw_stack_layers', type=int, default=2, help='random walk module stack layers')
+    parser.add_argument('--rw_dropout', type=float, default=0.3, help='random walk dropout rate')
+    parser.add_argument('--NGCF_layers', type=int, default=3, help='ngcf layers')
+    parser.add_argument('--ngcf_dropout', type=float, default=0.3, help='ngcf dropout rate')
+    
+    return parser.parse_args()
 
 # TODO: load from file
+args = parse_args()
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-data_generator = Data(batch_size=8192, random_walk_length=16,device=device)
+data_generator = Data(batch_size=args.batch_size, random_walk_length=16,device=device)
 pretrained_author_embedding = data_generator.author_embeddings
 pretrained_paper_embedding = data_generator.paper_embeddings
 
@@ -136,28 +152,21 @@ def train(model, optimizer, args):
         save_checkpoint(model, args.save_dir, args.keep_last_epochs, epoch_total_recall / n_train_batch, epoch_idx)
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(description="Run General")
-    parser.add_argument('save_dir', type=str, default='checkpoints', help='Directory to save checkpoints')
-    parser.add_argument('--module_type', nargs='?', default='LSTM', help='Module in coauthor and citation network')
-    parser.add_argument('--lr', type=float, default=0.0001, help='Learning rate.')
-    parser.add_argument('--epoch', type=int, default=100, help='Number of epochs')
-    parser.add_argument('--keep_last_epochs', type=int, default=5, help='only keep last epochs')
-    return parser.parse_args()
+
 
 if __name__ == '__main__':
-    args = parse_args()
+    
     model = General(
-        RWembed_dim=512,
-        stack_layers=2,
-        dropoutRW=0.3,
+        RWembed_dim=args.embed_dim,
+        stack_layers=args.rw_stack_layers,
+        dropoutRW=args.rw_dropout,
         n_authors=data_generator.n_authors,
         n_papers=data_generator.n_papers,
-        num_layers=2,
-        NGCFembed_dim=512,
-        dropoutNGCF=0.3,
-        paper_dim=512,
-        author_dim=512,
+        num_layers=args.NGCF_layers,
+        NGCFembed_dim=args.embed_dim,
+        dropoutNGCF=args.ngcf_dropout,
+        paper_dim=args.embed_dim,
+        author_dim=args.embed_dim,
         norm_adj=data_generator.bipartite_lap_matrix,
         n_fold=4,
         args=args
